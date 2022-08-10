249130af244c6603a5da6fb871181d7a7c6a2649
junjange <wnswkd486@naver.com>
2022-08-10 15:12:51 +0900

    Create 2557.java

---
+++ baekjoon/implementation/2557.java
@@ -0,0 +1,12 @@
+
+package level1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String [] args) {
+		System.out.println("Hello World!");
+	}
+}
